70a542e43415a68fabfa8cccf62be977b725423d
killer <michael.molchanov.2004@gmail.com>
2022-09-08 15:22:55 +0200

    added meta

--- web/lab/tasks/1-08.09.2022/html-practice/src/app/app.component.ts
+++ web/lab/tasks/1-08.09.2022/html-practice/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Meta} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent
+{
   title = 'html-practice';
+
+  constructor(private meta: Meta)
+  {
+    this.meta.addTags([
+      {name: 'description', content: 'This is first laboratory assignment'},
+      {name: 'author', content: 'Mike Molchanov'},
+      {name: 'keywords', content: 'Angular Material KPI Laboratory Assignment One First'},
+    ]);
+  }
 }

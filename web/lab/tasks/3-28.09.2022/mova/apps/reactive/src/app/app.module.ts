13732f4c8096e3fe67e48634894d82874e3d08b8
killer <michael.molchanov.2004@gmail.com>
2022-09-28 22:53:23 +0200

    footer

--- web/lab/tasks/3-28.09.2022/mova/apps/reactive/src/app/app.module.ts
+++ web/lab/tasks/3-28.09.2022/mova/apps/reactive/src/app/app.module.ts
@@ -1,37 +1,38 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {NxWelcomeComponent} from './nx-welcome.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {StoreModule} from '@ngrx/store';
-import {EffectsModule} from '@ngrx/effects';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '../environments/environment';
-import {MainNavComponent} from './pres/main-nav/main-nav.component';
-import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {HomeComponent} from './pres/home/home.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { MainNavComponent } from './pres/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './pres/home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import * as fromAppState from './store';
 import * as fromView from './store/reducers/view/view.reducer';
-import {reducer} from './store/reducers/view/view.reducer';
-import {HomeCoreComponent} from './cont/home-core/home-core.component';
-import {NavCoreComponent} from './cont/nav-core/nav-core.component';
+import { reducer } from './store/reducers/view/view.reducer';
+import { HomeCoreComponent } from './cont/home-core/home-core.component';
+import { NavCoreComponent } from './cont/nav-core/nav-core.component';
 import * as fromLink from './store/reducers/link/link.reducer';
-import {RouterModule, Routes} from '@angular/router';
-import {LoadingComponent} from './pres/loading/loading.component';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {RoutingEffects} from './store/effects/routing/routing.effects';
+import { RouterModule, Routes } from '@angular/router';
+import { LoadingComponent } from './pres/loading/loading.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RoutingEffects } from './store/effects/routing/routing.effects';
+import { FooterComponent } from './pres/footer/footer.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeCoreComponent},
-  {path: 'loading', component: LoadingComponent},
+  { path: 'home', component: HomeCoreComponent },
+  { path: 'loading', component: LoadingComponent },
 ];
 
 @NgModule({
@@ -43,6 +44,7 @@ const routes: Routes = [
     HomeCoreComponent,
     NavCoreComponent,
     LoadingComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -67,5 +69,4 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
